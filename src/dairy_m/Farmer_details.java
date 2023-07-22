a45b233dd4df43e6979ecca6da825a03ae2af86a
Namit Khushal Yadav <yadavnamit605@gmail.com>
2023-07-22 17:21:42 +0530

    Existing details work is ongoing

--- src/dairy_m/Farmer_details.java
+++ src/dairy_m/Farmer_details.java
@@ -4,6 +4,12 @@
  */
 package dairy_m;
 
+import java.awt.event.ActionEvent;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 91975
@@ -63,6 +69,9 @@ public class Farmer_details extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated
     // Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -70,12 +79,11 @@ public class Farmer_details extends javax.swing.JDialog {
         back = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
-        jLabel2 = new javax.swing.JLabel();
-        Farmerid = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         farmername = new javax.swing.JTextField();
+        clear = new javax.swing.JButton();
         search = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -150,27 +158,29 @@ public class Farmer_details extends javax.swing.JDialog {
 
         getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 193, 840, 318));
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
-        jLabel2.setText("Farmer_ID");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 90, -1, 20));
-
-        Farmerid.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                FarmeridActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Farmerid, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 120, 151, 40));
-
         jLabel3.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
         jLabel3.setText("Farmer_Name");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 90, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 80, -1, -1));
 
         farmername.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                actionPerformed(evt);
+                farmernameActionPerformed(evt);
+            }
+
+            private void farmernameActionPerformed(ActionEvent evt) {
             }
         });
-        getContentPane().add(farmername, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 120, 151, 40));
+        getContentPane().add(farmername, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 120, 151, 40));
+
+        clear.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        clear.setText("Clear");
+        clear.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        clear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearActionPerformed(evt);
+            }
+        });
+        getContentPane().add(clear, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 140, 70, -1));
 
         search.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
         search.setText("Search");
@@ -180,26 +190,32 @@ public class Farmer_details extends javax.swing.JDialog {
                 searchActionPerformed(evt);
             }
         });
-        getContentPane().add(search, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 130, 70, -1));
+        getContentPane().add(search, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 90, 70, -1));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/new.jpg"))); // NOI18N
-        jLabel5.setText("jLabel5");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(-3, 0, 840, 510));
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/new.jpg"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(-3, -10, 840, 490));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void clearActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_clearActionPerformed
+        farmername.setText(null);
+    }// GEN-LAST:event_clearActionPerformed
+
     private void searchActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_searchActionPerformed
-        // TODO add your handling code here:
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dairy_management_system",
+                    "root", "2412");
+            String namit = "select * from farmer where farmer_name = ?";
+            PreparedStatement pstmt = con.prepareStatement(namit);
+
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e, null, 0);
+        }
+
     }// GEN-LAST:event_searchActionPerformed
-
-    private void FarmeridActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_FarmeridActionPerformed
-        // TODO add your handling code here:
-    }// GEN-LAST:event_FarmeridActionPerformed
-
-    private void Farmerid1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_Farmerid1ActionPerformed
-        // TODO add your handling code here:
-    }// GEN-LAST:event_Farmerid1ActionPerformed
 
     private void backActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_backActionPerformed
         Farmer f = new Farmer();
@@ -212,13 +228,12 @@ public class Farmer_details extends javax.swing.JDialog {
      */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField Farmerid;
     private javax.swing.JButton back;
+    private javax.swing.JButton clear;
     private javax.swing.JTextField farmername;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton search;
     private javax.swing.JTable table;
